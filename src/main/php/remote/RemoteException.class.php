72e680c141f4a101a779f916043ee1b310335ff3
No author <xp-framework-null@xp-framework.de>
1999-08-19 14:09:20 +0000

    New repository initialized by cvs2svn.
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@1 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ src/main/php/remote/RemoteException.class.php
@@ -0,0 +1,16 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  /**
+   * Indicates an exception occured on the server
+   *
+   * @see      xp://Remote
+   * @purpose  Exception
+   */
+  class RemoteException extends XPException {
+
+  }
+?>
---
+++ src/main/php/remote/beans/BeanInterface.class.php
@@ -0,0 +1,17 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  define('STATELESS', 1);
+
+  /**
+   * Bean Interface
+   *
+   * @purpose  Base interface
+   */
+  interface BeanInterface {
+  
+  }
+?>
